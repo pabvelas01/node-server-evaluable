0069922c3f30955b94ae6935c7dfe3fc0694276c
Pablo Velasquez <pvelasquez@ubiobio.cl>
2023-01-13 21:39:37 -0300

    Agrega cors server

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express =require('express');
 const conectaDB= require('./config/db');
+const cors= require('cors');
 // creamos el servidor 
 const app= express(); 
 conectaDB();
 // sin esta linea no se puede ingresar json como input
 app.use(express.json());
+app.use(cors());
 app.use('/api/rol',require('./routes/rol'));
 app.listen(4000, ()=>{
     console.log("El servidor esta corriendo perfectamente");
