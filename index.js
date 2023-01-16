ded338ab6d70915b9b7adcaf34c4833499a776fa
Pablo Velasquez <pvelasquez@ubiobio.cl>
2023-01-16 10:17:57 -0300

    Agrega intento de ssh

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express =require('express');
 const conectaDB= require('./config/db');
 const cors= require('cors');
+const https= require('https');
+const fs= require('fs');
 // creamos el servidor 
 const app= express(); 
 conectaDB();
+const PUERTO=443;
 // sin esta linea no se puede ingresar json como input
 app.use(express.json());
 app.use(cors());
 app.use('/api/rol',require('./routes/rol'));
 app.use('/api/user',require('./routes/user'));
 app.use('/api/autentificacion',require('./routes/autentificacion'));
+/*
 app.listen(4000, ()=>{
     console.log("El servidor esta corriendo perfectamente");
 });
+*/
+https.createServer({
+    cert:fs.readFileSync('server.crt'),
+    key:fs.readFileSync('server.key')},app).listen(PUERTO,function(){
+        console.log("server corriendo https");
+    });
